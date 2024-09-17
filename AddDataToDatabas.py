eb4dd3f3ca738f6df7d84b0dbe7473846a097d7c
sup-uj <142181194+sup-uj@users.noreply.github.com>
2024-09-18 00:57:55 +0530

    Complete attendance logic and minor bug fixes

--- AddDataToDatabas.py
+++ AddDataToDatabas.py
@@ -84,3 +84,66 @@ while True:
                     modeType = 1
             if counter!=0:
 
+                if counter == 1:
+                    #get the data
+                    studentInfo = db.reference(f'Students/{id}').get()
+                    print(studentInfo)
+                    #Get the Image from storage bucket
+                    blob = bucket.get_blob(f'Images/{id}.png')
+                    array = np.frombuffer(blob.download_as_string(), np.uint8)
+                    imgStudent = cv2.imdecode(array,cv2.COLOR_BGRA2RGB)
+                    # update attendance
+                    datetimeObject = datetime.strptime(studentInfo['last_attendance_time'],"%Y-%m-%d %H:%M:%S")
+
+                    secondsElapsed = (datetime.now()-datetimeObject).total_seconds()
+                    #print(secondsElapsed)
+                    if secondsElapsed>30:
+                        ref = db.reference(f'Students/{id}')
+                        studentInfo['total_attendance'] += 1
+                        ref.child('total_attendance').set(studentInfo['total_attendance'])
+                        ref.child('last_attendance_time').set(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                    else:
+                        modeType=3
+                        counter=0
+                        imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
+                if modeType!=3:
+                    if 10<counter<=20:
+                        modeType=2
+
+                    imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
+
+                    if counter<=10:
+                        cv2.putText(imgBackground,str(studentInfo['total_attendance']),(861,125),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1)
+                        cv2.putText(imgBackground, str(studentInfo['major']), (1006,550), cv2.FONT_HERSHEY_COMPLEX, 0.5,
+                                    (255, 255, 255), 1)
+                        cv2.putText(imgBackground, str(id), (1006,493), cv2.FONT_HERSHEY_COMPLEX, 0.5,
+                                    (255, 255, 255), 1)
+                        cv2.putText(imgBackground, str(studentInfo['standing']), (910,625), cv2.FONT_HERSHEY_COMPLEX, 0.6,
+                                    (100,100,100), 1)
+                        cv2.putText(imgBackground, str(studentInfo['year']), (1025,625), cv2.FONT_HERSHEY_COMPLEX, 0.6,
+                                    (100,100,100), 1)
+                        (w, h), _ = cv2.getTextSize(studentInfo['name'], cv2.FONT_HERSHEY_COMPLEX, 1, 1)
+                        offset = (414-w)//2
+                        #'//' because offset mught be float
+                        cv2.putText(imgBackground, str(studentInfo['name']), (808+offset, 445), cv2.FONT_HERSHEY_COMPLEX, 1, (50, 50, 50),
+                                    1)
+
+                        imgBackground[175:175+216,909:909+216] = imgStudent
+
+                    counter+=1
+                #print("known face detected")
+                #print(studentIds[matchIndex])
+
+                    if counter>=20:
+                        counter = 0
+                        modeType=0
+                        studentInfo = []
+                        imgStudent = []
+                        imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
+
+    else:
+        modeType=0
+        counter=0
+    #cv2.imshow("Webcam", img)
+    cv2.imshow("Face Attendance", imgBackground)
+    cv2.waitKey(1)
